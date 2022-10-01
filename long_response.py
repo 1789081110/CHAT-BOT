df70132d3e6e05b447672ba52c8c3490096aefd5
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-10-01 18:46:49 +0530

    Create long_response.py

---
+++ long_response.py
@@ -0,0 +1,6 @@
+import random
+
+R_EATING="I don't like eating because i am a chat bot, not a human!"
+def unknown():
+    response=['Cant understand',"....", 'sounds absurd','what does that mean'][random.randrange(4)]
+    return response
